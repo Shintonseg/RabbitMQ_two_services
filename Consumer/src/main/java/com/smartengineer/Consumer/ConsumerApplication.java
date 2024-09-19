4547e82ca2d3ec4f487125c2c0b15f6f1577e17f
Shinton George <shintonseg5@gmail.com>
2024-09-19 22:49:51 +0530

    added rabbit listener changes

--- Consumer/src/main/java/com/smartengineer/Consumer/ConsumerApplication.java
+++ Consumer/src/main/java/com/smartengineer/Consumer/ConsumerApplication.java
@@ -1,5 +1,6 @@
 package com.smartengineer.Consumer;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +11,8 @@ public class ConsumerApplication {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
 
+	@RabbitListener(queues = "${rabbitmq.queue.name}")
+	public void receiveMessage(String message) {
+		System.out.println("Message received: " + message);
+	}
 }
